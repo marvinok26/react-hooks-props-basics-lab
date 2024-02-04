aa0fbaf4b9f2dece7bff4319215fe9a21743c033
Marvin <okongomarvin971@gmail.com>
2024-02-04 23:05:14 +0300

    Completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ function About(props) {
         <h2>About Me</h2>
         <p>{props.bio}</p>
         <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-        <Links/>
+        <Links github={user.links.github} linkedin={user.links.linkedin} />
       </div>
     );
   }
